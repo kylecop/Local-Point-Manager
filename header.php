5bddb2d4a2203f380b25c37d38f9ea292c2b8abb
kylecop <43895134+kylecop@users.noreply.github.com>
2019-06-28 13:51:32 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Beats Points Manager 6</title>
+  <title>Beats Points Manager 7</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">

c48c957b46a15aa726b293224d1347ad3d2dcccb
kylecop <43895134+kylecop@users.noreply.github.com>
2019-07-07 19:07:48 -0500

    Add files via upload

--- displaySettings.php
+++ displaySettings.php
@@ -13,6 +13,7 @@ function displaySettings(){
   echo "<br>Number of Coins Required: ".getSqlString("SELECT value FROM settings WHERE `settingName`='numCoinsRequired'");
   echo "<form action=\"index.php\" method=\"post\"><input type=\"submit\" name=\"DisableSessions\" value=\"Disable Sessions\" /></form>";
   echo "<form action=\"index.php\" method=\"post\"><input type=\"submit\" name=\"EnableSessions\" value=\"Enable Sessions\" /></form>";
+  echo "<form action=\"uploadForm.php\" method=\"post\"><input type=\"submit\" name=\"uploadForm\" value=\"Upload Konnect\" /></form>";
 
   //echo "<br><button type=\"button\" class=\"btn btn-primary\">Enable Sessions</button>";
   //echo "<br><button type=\"button\" class=\"btn btn-primary\">Disable Sessions</button>";
---
+++ upload.php
@@ -0,0 +1,99 @@
+<?php
+include('postToSQL.php');
+$target_dir = "uploads/";
+$target_file = $target_dir  . date("YmdHis") . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        resizeImage($target_file, $target_file, 1024, 1024);
+
+        postToSqlComputerSettings("settings", $target_file, "currentPicture");
+
+        /**
+         * Example
+         * resizeImage('image.jpg', 'resized.jpg', 200, 200);
+         */
+
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+function resizeImage($sourceImage, $targetImage, $maxWidth, $maxHeight, $quality = 80)
+{
+    // Obtain image from given source file.
+    if (!$image = @imagecreatefromjpeg($sourceImage))
+    {
+        return false;
+    }
+
+    // Get dimensions of source image.
+    list($origWidth, $origHeight) = getimagesize($sourceImage);
+
+    if ($maxWidth == 0)
+    {
+        $maxWidth  = $origWidth;
+    }
+
+    if ($maxHeight == 0)
+    {
+        $maxHeight = $origHeight;
+    }
+
+    // Calculate ratio of desired maximum sizes and original sizes.
+    $widthRatio = $maxWidth / $origWidth;
+    $heightRatio = $maxHeight / $origHeight;
+
+    // Ratio used for calculating new image dimensions.
+    $ratio = min($widthRatio, $heightRatio);
+
+    // Calculate new image dimensions.
+    $newWidth  = (int)$origWidth  * $ratio;
+    $newHeight = (int)$origHeight * $ratio;
+
+    // Create final image with new dimensions.
+    $newImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($newImage, $image, 0, 0, 0, 0, $newWidth, $newHeight, $origWidth, $origHeight);
+    imagejpeg($newImage, $targetImage, $quality);
+
+    // Free up the memory.
+    imagedestroy($image);
+    imagedestroy($newImage);
+
+    return true;
+}
+
+
+?>
